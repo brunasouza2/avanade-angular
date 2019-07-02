b101e4b00e6fb79e8bad156acc88b3bff154c188
brunasouza2 <50778590+brunasouza2@users.noreply.github.com>
2019-07-01 21:43:38 -0300

    Primeiro commit
    Signed-off-by: brunasouza2 <50778590+brunasouza2@users.noreply.github.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'gama-angular';
+export class AppComponent implements OnInit {
+  title = 'Bruna';
+  itens = ['7','3','8','1','0'];
+
+  ngOnInit(){
+
+    setTimeout(() => {
+      this.title = "Souza";
+    }, 3000)
+    
+  }
+
+  novoItem(){
+    const text = prompt('Digite um nome');
+    this.itens.push(text);
+  }
 }
